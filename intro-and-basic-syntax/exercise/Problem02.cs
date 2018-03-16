1b489ac739ef76279976dccc5d79dbec22dad192
RadiRadev <raddy.radev@gmail.com>
2018-03-16 20:51:22 +0200

    var changes

--- intro-and-basic-syntax/exercise/Problem02.cs
+++ intro-and-basic-syntax/exercise/Problem02.cs
@@ -8,16 +8,16 @@ namespace Softuni
         static void Main(string[] args)
         {
             //We recieve 2 floating point numbers-double
-            var n1 =double.Parse(Console.ReadLine());
-            var n2= double.Parse(Console.ReadLine());
+            var width = double.Parse(Console.ReadLine());
+            var height = double.Parse(Console.ReadLine());
 
             /*We have to calculate the rectangle’s area,
              * based on its width and height*/
-            var result = n1 *n2;
+            var area = width*height;
 
             /*We print the result formatted to 
              * the 2nd character after the decimal point*/
-            Console.WriteLine($"{result:f2}");
+            Console.WriteLine($"{area:f2}");
         }
     }
 }
