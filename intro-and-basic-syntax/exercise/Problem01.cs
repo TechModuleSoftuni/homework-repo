3ad0ef45e4ea447a443c2d58ef0d77a49fead04a
KalinaPetrova <kiki_kirova@abv.bg>
2018-03-16 15:14:04 +0200

    Add documents the code for Problem01.cs and Problem02.cs

--- intro-and-basic-syntax/exercise/Problem01.cs
+++ intro-and-basic-syntax/exercise/Problem01.cs
@@ -7,11 +7,17 @@ namespace Problem01
     {
         static void Main(string[] args)
         {
+            //We recieve from the Consol 4 itegers
             var n1 = int.Parse(Console.ReadLine());
             var n2 = int.Parse(Console.ReadLine());
             var n3 = int.Parse(Console.ReadLine());
             var n4 = int.Parse(Console.ReadLine());
 
+            /*We have to print the input integers
+              in 4-digit format as debit card
+              (example: input->12 output->0012)
+              the result must be in one line
+              split by space*/
             Console.Write($"{n1:d4} ");
             Console.Write($"{n2:d4} ");
             Console.Write($"{n3:d4} ");
--- intro-and-basic-syntax/exercise/Problem02.cs
+++ intro-and-basic-syntax/exercise/Problem02.cs
@@ -7,13 +7,17 @@ namespace Softuni
     {
         static void Main(string[] args)
         {
+            //We recieve 2 floating point numbers-double
             var n1 =double.Parse(Console.ReadLine());
             var n2= double.Parse(Console.ReadLine());
+
+            /*We have to calculate the rectangle’s area,
+             * based on its width and height*/
             var result = n1 *n2;
-           
+
+            /*We print the result formatted to 
+             * the 2nd character after the decimal point*/
             Console.WriteLine($"{result:f2}");
-           
-            
         }
     }
 }
