a8efbdcf571ab19cd37e4cb8948506e373275865
KalinaPetrova <kiki_kirova@abv.bg>
2018-03-16 14:29:32 +0200

    Add Problem 4-BeverageLabels.cs and Problem03-MilesToKm.cs

---
+++ intro-and-basic-syntax/exercise/Problem 4-BeverageLabels.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Problem_4.Beverage_Labels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string bevarageName = Console.ReadLine();
+            int inputVolume = int.Parse(Console.ReadLine());
+            double inputEnergiContent = double.Parse(Console.ReadLine());
+            double inputSugarContent = double.Parse(Console.ReadLine());
+                        
+            double energi = inputEnergiContent/100;
+            double sugar = inputSugarContent/100;
+            double energiContentFin = energi * inputVolume;
+            double sugarContentFin = sugar * inputVolume;
+
+            /*220ml Nuka-Cola:
+              660kcal, 154g sugars*/
+            
+            Console.WriteLine($"{inputVolume}ml {bevarageName}:");
+            Console.WriteLine($"{energiContentFin}kcal, {sugarContentFin}g sugars");
+
+        }
+    }
+}
---
+++ intro-and-basic-syntax/exercise/Problem03-MilesToKm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Problem_3.Miles_to_Kilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 mile == 1.60934 kilometers
+            double miles = double.Parse(Console.ReadLine());
+            double kilometers = miles * 1.60934;
+            
+            Console.WriteLine($"{kilometers:F2}");
+        }
+    }
+}
