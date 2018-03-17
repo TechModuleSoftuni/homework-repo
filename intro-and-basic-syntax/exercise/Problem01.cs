1b83dabf7b2012310e5cd79804072a585866143b
RosenDev <33036545+RosenDev@users.noreply.github.com>
2018-03-17 16:36:26 +0200

    changed variables name

--- intro-and-basic-syntax/exercise/Problem01.cs
+++ intro-and-basic-syntax/exercise/Problem01.cs
@@ -8,20 +8,20 @@ namespace Problem01
         static void Main(string[] args)
         {
             //We recieve from the Consol 4 itegers
-            var n1 = int.Parse(Console.ReadLine());
-            var n2 = int.Parse(Console.ReadLine());
-            var n3 = int.Parse(Console.ReadLine());
-            var n4 = int.Parse(Console.ReadLine());
+            var number1 = int.Parse(Console.ReadLine());
+            var number2 = int.Parse(Console.ReadLine());
+            var number3 = int.Parse(Console.ReadLine());
+            var number4 = int.Parse(Console.ReadLine());
 
             /*We have to print the input integers
               in 4-digit format as debit card
               (example: input->12 output->0012)
               the result must be in one line
               split by space*/
-            Console.Write($"{n1:d4} ");
-            Console.Write($"{n2:d4} ");
-            Console.Write($"{n3:d4} ");
-            Console.Write($"{n4:d4} ");
+            Console.Write($"{number1:d4} ");
+            Console.Write($"{number2:d4} ");
+            Console.Write($"{number3:d4} ");
+            Console.Write($"{number4:d4} ");
 
         }
     }
