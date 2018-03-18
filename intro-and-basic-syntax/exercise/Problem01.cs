9d685c4291f20c1419df5f8c79857b699748514e
KalinaPetrova <kiki_kirova@abv.bg>
2018-03-18 17:55:21 +0200

    add changes to the variables in Problem01.cs and Problem02.cs

--- intro-and-basic-syntax/exercise/Problem01.cs
+++ intro-and-basic-syntax/exercise/Problem01.cs
@@ -8,20 +8,20 @@ namespace Problem01
         static void Main(string[] args)
         {
             //We recieve from the Consol 4 itegers
-            var number1 = int.Parse(Console.ReadLine());
-            var number2 = int.Parse(Console.ReadLine());
-            var number3 = int.Parse(Console.ReadLine());
-            var number4 = int.Parse(Console.ReadLine());
+            var firstNumber = int.Parse(Console.ReadLine());
+            var secondNumber = int.Parse(Console.ReadLine());
+            var thirdNumber = int.Parse(Console.ReadLine());
+            var fourthNumber = int.Parse(Console.ReadLine());
 
             /*We have to print the input integers
               in 4-digit format as debit card
               (example: input->12 output->0012)
               the result must be in one line
               split by space*/
-            Console.Write($"{number1:d4} ");
-            Console.Write($"{number2:d4} ");
-            Console.Write($"{number3:d4} ");
-            Console.Write($"{number4:d4} ");
+            Console.Write($"{firstNumber:d4} ");
+            Console.Write($"{secondNumber:d4} ");
+            Console.Write($"{thirdNumber:d4} ");
+            Console.Write($"{fourthNumber:d4} ");
 
         }
     }
--- intro-and-basic-syntax/exercise/Problem02.cs
+++ intro-and-basic-syntax/exercise/Problem02.cs
@@ -8,16 +8,16 @@ namespace Softuni
         static void Main(string[] args)
         {
             //We recieve 2 floating point numbers-double
-            var width = double.Parse(Console.ReadLine());
-            var height = double.Parse(Console.ReadLine());
+            var widthRectangle = double.Parse(Console.ReadLine());
+            var heightRectangle = double.Parse(Console.ReadLine());
 
             /*We have to calculate the rectangle’s area,
              * based on its width and height*/
-            var area = width*height;
+            var areaRectangle = widthRectangle*heightRectangle;
 
             /*We print the result formatted to 
              * the 2nd character after the decimal point*/
-            Console.WriteLine($"{area:f2}");
+            Console.WriteLine($"{areaRectangle:f2}");
         }
     }
 }
