26903745b07a0c33309668a0eabb82192c9dfa66
Radi Radev <33155474+ketobeast86@users.noreply.github.com>
2018-03-22 07:45:40 +0200

    ###Actual behavior:A compile time error occurred;Problem with variable 'double sugar'-resolved###expected behavior - accomplished;
    ###Actual behavior:A compile time error occurred;Problem with variable 'double sugar'-resolved###expected behavior - accomplished;

--- intro-and-basic-syntax/exercise/Problem 4-BeverageLabels.cs
+++ intro-and-basic-syntax/exercise/Problem 4-BeverageLabels.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Problem_4.Beverage_Labels
 {
@@ -10,18 +10,18 @@ namespace Problem_4.Beverage_Labels
             int volume = int.Parse(Console.ReadLine());
             double energyContentPer100ml = double.Parse(Console.ReadLine());
             double sugarContentPer100ml = double.Parse(Console.ReadLine());
-                        
-            double energy = energyContentPer100ml/100;
-            double sugar = sugarContentPe100ml/100;
+
+            double energy = energyContentPer100ml / 100;
+            double sugar = sugarContentPer100ml / 100;
             double totalEnergy = energy * volume;
             double totalSugar = sugar * volume;
 
             /*220ml Nuka-Cola:
               660kcal, 154g sugars*/
-            
+
             Console.WriteLine($"{volume}ml {productName}:");
             Console.WriteLine($"{totalEnergy}kcal, {totalSugar}g sugars");
- 
+
         }
     }
 }
